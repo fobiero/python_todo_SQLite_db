d7b5b3f416fc82ef8ebca597c16de3c5fcdd4cf1
feLycks <felix.obiero@student.moringaschool.com>
2022-01-19 20:47:42 +0300

    finish Logic build

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
 // alert('env setUp')
+
+function getData() {
+    const name = document.getElementById('name').value;
+
+    if (name == '') {
+        alert('Field Required')
+    }
+    return true
+}
